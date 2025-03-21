b20fa165707cac73f6a538823d278c203765647d
EmaSevcikova <emkasevcikova@gmail.com>
2025-03-21 13:16:12 +0100

    refactor: signature analysis logic moved to separate directory

--- analyzer/context_analyzer.py
+++
@@ -1,8 +0,0 @@
-
-def parse_maps(pid):
-    with open(f"/proc/{pid}/maps", "r") as f:
-        return f.readlines()
-
-def get_process_context(pid):
-    with open(f"/proc/{pid}/status", "r") as f:
-        return f.read()
